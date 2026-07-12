4b07dd88f13d1c25099e198de78f4d441ed24f71
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:39 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,33 +1,29 @@
-output "spring_cloud_java_deployments" {
-  description = "All spring_cloud_java_deployment resources"
-  value       = azurerm_spring_cloud_java_deployment.spring_cloud_java_deployments
-}
 output "spring_cloud_java_deployments_environment_variables" {
-  description = "List of environment_variables values across all spring_cloud_java_deployments"
-  value       = [for k, v in azurerm_spring_cloud_java_deployment.spring_cloud_java_deployments : v.environment_variables]
+  description = "Map of environment_variables values across all spring_cloud_java_deployments, keyed the same as var.spring_cloud_java_deployments"
+  value       = { for k, v in azurerm_spring_cloud_java_deployment.spring_cloud_java_deployments : k => v.environment_variables }
 }
 output "spring_cloud_java_deployments_instance_count" {
-  description = "List of instance_count values across all spring_cloud_java_deployments"
-  value       = [for k, v in azurerm_spring_cloud_java_deployment.spring_cloud_java_deployments : v.instance_count]
+  description = "Map of instance_count values across all spring_cloud_java_deployments, keyed the same as var.spring_cloud_java_deployments"
+  value       = { for k, v in azurerm_spring_cloud_java_deployment.spring_cloud_java_deployments : k => v.instance_count }
 }
 output "spring_cloud_java_deployments_jvm_options" {
-  description = "List of jvm_options values across all spring_cloud_java_deployments"
-  value       = [for k, v in azurerm_spring_cloud_java_deployment.spring_cloud_java_deployments : v.jvm_options]
+  description = "Map of jvm_options values across all spring_cloud_java_deployments, keyed the same as var.spring_cloud_java_deployments"
+  value       = { for k, v in azurerm_spring_cloud_java_deployment.spring_cloud_java_deployments : k => v.jvm_options }
 }
 output "spring_cloud_java_deployments_name" {
-  description = "List of name values across all spring_cloud_java_deployments"
-  value       = [for k, v in azurerm_spring_cloud_java_deployment.spring_cloud_java_deployments : v.name]
+  description = "Map of name values across all spring_cloud_java_deployments, keyed the same as var.spring_cloud_java_deployments"
+  value       = { for k, v in azurerm_spring_cloud_java_deployment.spring_cloud_java_deployments : k => v.name }
 }
 output "spring_cloud_java_deployments_quota" {
-  description = "List of quota values across all spring_cloud_java_deployments"
-  value       = [for k, v in azurerm_spring_cloud_java_deployment.spring_cloud_java_deployments : v.quota]
+  description = "Map of quota values across all spring_cloud_java_deployments, keyed the same as var.spring_cloud_java_deployments"
+  value       = { for k, v in azurerm_spring_cloud_java_deployment.spring_cloud_java_deployments : k => v.quota }
 }
 output "spring_cloud_java_deployments_runtime_version" {
-  description = "List of runtime_version values across all spring_cloud_java_deployments"
-  value       = [for k, v in azurerm_spring_cloud_java_deployment.spring_cloud_java_deployments : v.runtime_version]
+  description = "Map of runtime_version values across all spring_cloud_java_deployments, keyed the same as var.spring_cloud_java_deployments"
+  value       = { for k, v in azurerm_spring_cloud_java_deployment.spring_cloud_java_deployments : k => v.runtime_version }
 }
 output "spring_cloud_java_deployments_spring_cloud_app_id" {
-  description = "List of spring_cloud_app_id values across all spring_cloud_java_deployments"
-  value       = [for k, v in azurerm_spring_cloud_java_deployment.spring_cloud_java_deployments : v.spring_cloud_app_id]
+  description = "Map of spring_cloud_app_id values across all spring_cloud_java_deployments, keyed the same as var.spring_cloud_java_deployments"
+  value       = { for k, v in azurerm_spring_cloud_java_deployment.spring_cloud_java_deployments : k => v.spring_cloud_app_id }
 }
 
